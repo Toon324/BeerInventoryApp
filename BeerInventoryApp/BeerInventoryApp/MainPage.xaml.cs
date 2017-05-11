ad4b83912720b59016ed6eb833ec3ccf464197ac
Cody Swendrowski <cody.swendrowski@7summitsinc.com>
2017-05-11 00:56:11 -0500

    Adds basic search feature

--- BeerInventoryApp/BeerInventoryApp/MainPage.xaml.cs
+++ BeerInventoryApp/BeerInventoryApp/MainPage.xaml.cs
@@ -16,18 +16,15 @@ namespace BeerInventoryApp
 
         InventoryService InventoryService { get; set; } = new InventoryService();
 
+        private String name = "Cody";
+
+        private int lastResultCount = 0;
+
         public MainPage()
         {
             InitializeComponent();
 
-            var inventory = InventoryService.GetInventory("Cody");
-
-            foreach (var beer in inventory)
-            {
-                Items.Add(beer);
-            }
-
-            listView.ItemsSource = Items;
+            UpdateItems();
         }
 
 
@@ -36,6 +33,74 @@ namespace BeerInventoryApp
             var name = e.SelectedItem.ToString();
 
             editor.Text = name;
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            name = e.NewTextValue;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            name = this.FindByName<Entry>("Name_Entry").Text;
+            UpdateItems();
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            name = ((Entry)sender).Text;
+            UpdateItems();
+        }
+
+        private void UpdateItems()
+        {
+            if (string.IsNullOrEmpty(name))
+                return;
+
+            var inventory = InventoryService.GetInventory(name);
+
+            var newList = new ObservableCollection<InventoryDetails>();
+
+            foreach (var beer in inventory)
+            {
+                newList.Add(beer);
+            }
+
+            Items = newList;
+
+            listView.ItemsSource = Items;
+            lastResultCount = Items.Count();
+        }
+
+        private void searchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            var searchTerm = ((SearchBar)sender).Text.ToLower();
+
+            UpdateSearchResults(searchTerm);
+        }
+
+        private void searchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchTerm = e.NewTextValue.ToLower();
+
+            UpdateSearchResults(searchTerm);
+        }
+
+        private void UpdateSearchResults(String searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                listView.ItemsSource = Items;
+            }
+
+            var results = new ObservableCollection<InventoryDetails>(Items.Where(x => x.Name.ToLower().Contains(searchTerm) || searchTerm.ToLower().Contains(x.Name)).ToList());
+
+            // TODO: Probably hacky, should rework
+            if (results.Count != lastResultCount)
+            {
+                lastResultCount = results.Count;
+                listView.ItemsSource = results;
+            }
         }
     }
 }
