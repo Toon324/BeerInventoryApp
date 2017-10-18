143ccdeba2e3495035bc5c30756b84d66e6766f3
Cody Swendrowski <cody@swendrowski.us>
2017-10-18 14:23:56 -0500

    Removes unnecessary and outdated ApiKey definition

--- BeerInventoryApp/BeerInventoryApp/RestInterfaces/IAzureSearchApi.cs
+++ BeerInventoryApp/BeerInventoryApp/RestInterfaces/IAzureSearchApi.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace BeerInventoryApp.RestInterfaces
 {
-    [Headers("api-key: " + AzureSearchApi.ApiKey, "Accept: application/json")]
     public interface IAzureSearchApi
     {
         [Get("/{index}/docs?search={search}&api-version=2016-09-01")]
@@ -20,8 +19,6 @@ namespace BeerInventoryApp.RestInterfaces
     public static class AzureSearchApi
     {
         public const string ApiUrl = "https://beerinventory.search.windows.net/indexes";
-
-        public const string ApiKey = "D21A03E75C738E3351A921EA306F13F4";
 
         public const string BeerIndex = "beerindex";
     }
